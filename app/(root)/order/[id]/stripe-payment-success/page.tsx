225532dff5c3e00e63bcddf91ea736371b310bc0
JoBaHP <55216493+JoBaHP@users.noreply.github.com>
2025-01-13 14:45:47 +0100

    Versioning untracked files

--- app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -1,49 +1,53 @@
-import { Button } from '@/components/ui/button';
+import { Metadata } from 'next';
 import { getOrderById } from '@/lib/actions/order.actions';
-import Link from 'next/link';
-import { notFound, redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
+import OrderDetailsTable from './order-details-table';
+import { ShippingAddress } from '@/types';
+import { auth } from '@/auth';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+export const metadata: Metadata = {
+  title: 'Order Details',
+};
 
-const SuccessPage = async (props: {
-  params: Promise<{ id: string }>;
-  searchParams: Promise<{ payment_intent: string }>;
+const OrderDetailsPage = async (props: {
+  params: Promise<{
+    id: string;
+  }>;
 }) => {
   const { id } = await props.params;
-  const { payment_intent: paymentIntentId } = await props.searchParams;
 
-  // Fetch order
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  // Retrieve payment intent
-  const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+  const session = await auth();
 
-  // Check if payment intent is valid
-  if (
-    paymentIntent.metadata.orderId == null ||
-    paymentIntent.metadata.orderId !== order.id.toString()
-  ) {
-    return notFound();
+  let client_secret = null;
+
+  // Check if is not paid and using stripe
+  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+    // Init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    // Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: 'USD',
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
   }
 
-  // Check if payment is successful
-  const isSuccess = paymentIntent.status === 'succeeded';
-
-  if (!isSuccess) return redirect(`/order/${id}`);
-
   return (
-    <div className='max-w-4xl w-full mx-auto space-y-8'>
-      <div className='flex flex-col gap-6 items-center'>
-        <h1 className='h1-bold'>Thanks for your purchase</h1>
-        <div>We are processing your order.</div>
-        <Button asChild>
-          <Link href={`/order/${id}`}>View Order</Link>
-        </Button>
-      </div>
-    </div>
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+      stripeClientSecret={client_secret}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+      isAdmin={session?.user?.role === 'admin' || false}
+    />
   );
 };
 
-export default SuccessPage;
+export default OrderDetailsPage;
