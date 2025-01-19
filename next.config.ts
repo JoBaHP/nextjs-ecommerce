b991f4c22f36d8987437063f687e5f02daa9040d
JoBaHP <55216493+JoBaHP@users.noreply.github.com>
2025-01-19 11:40:43 +0100

    Added resend and bug fixed

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-     formats: ["image/avif", "image/webp"],
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'utfs.io',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
         port: '',
       },
     ],
